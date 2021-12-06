fcbbc735eeccd6a9c4189b9be4f95d5b43998d44
Gergosz-2000 <gergoszivak@gmail.com>
2021-12-06 11:14:27 +0100

    expo App kesz

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [radius, setRadius] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [area, setVolume] = useState(0);
+
+  function calcVolume(){
+    let area = Math.pow(radius,2)*Math.PI;
+    let result = area/3*height;
+    setVolume(parseFloat(result).toFixed(2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Térkúp</Text>
+      <View style={styles.start}>
+        <Text style={styles.inputTitle}>Sugár:</Text>
+        <TextInput placeholder="Sugár" style={styles.input} onChangeText={ radius => setRadius(radius) } />
+        <Text style={styles.inputTitle}>Magasság:</Text>
+        <TextInput placeholder="Magasság" style={styles.input} onChangeText={ height => setHeight(height) } />
+        <StatusBar style="auto" />
+        <Button title="Számít" color="#3b5998"
+        onPress={calcVolume}/>
+      </View>
+      <Text style={[styles.inputTitle,{fontSize: 40}]}>Térfogat: {area}</Text>
     </View>
   );
 }
@@ -14,8 +33,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dfe3ee',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  start: {
+    alignItems: 'flex-start',
+    backgroundColor: 'gray',
+    paddingHorizontal: 80,
+    paddingBottom: 20,
+    width: '100%',
+  },
+  input: {
+    borderWidth: 5,
+    borderColor: '#3b5998',
+    width: '100%',
+    marginTop: 8,
+    marginBottom: 8,
+    backgroundColor: 'white',
+    paddingStart: 10,
+  },
+  title: {
+    fontFamily: "Cochin",
+    color: "#3b5998",
+    margin: 40,
+    fontSize: 80,
+    fontWeight: "bold"
+  },
+  inputTitle: {
+    fontWeight: 'bold',
+    color: 'darkred',
+    marginTop: 20,
+  }
 });
